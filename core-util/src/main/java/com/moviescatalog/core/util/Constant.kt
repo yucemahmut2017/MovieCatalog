780a003015490a5960f99f35e1bac74adfa8cf33
mahmutyuce <yuce.mahmut2017@yandex.com>
2025-03-30 14:01:18 +0300

    Add core-util module and Constants.kt

---
+++ core-util/src/main/java/com/moviescatalog/core/util/Constant.kt
@@ -0,0 +1,6 @@
+package com.moviescatalog.core.util
+
+object Constants {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Movies Catalog"
 include(":app")
+include(":core-util")
